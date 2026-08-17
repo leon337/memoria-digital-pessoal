5c755a6aa90d07e7407c7c2e66f34f845d419b2a
Leandro Carlos <leonpcsn@gmail.com>
2026-08-17 07:21:58 +0200

    feat(api): add correction application errors

--- apps/api/src/memories/memory.errors.ts
+++ apps/api/src/memories/memory.errors.ts
@@ -4,3 +4,24 @@ export class MemoryInvariantError extends Error {
     this.name = 'MemoryInvariantError';
   }
 }
+
+export class MemoryNotFoundError extends Error {
+  constructor() {
+    super('Memory not found');
+    this.name = 'MemoryNotFoundError';
+  }
+}
+
+export class StaleCorrectionError extends Error {
+  constructor(readonly currentFactId: string) {
+    super('Memory changed since it was read');
+    this.name = 'StaleCorrectionError';
+  }
+}
+
+export class NoChangeCorrectionError extends Error {
+  constructor() {
+    super('Correction does not change current memory text');
+    this.name = 'NoChangeCorrectionError';
+  }
+}
